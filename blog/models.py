ca4397a225d4f0b2d677ac97da0d75034a6d02c8
KPEdit <kingprogramedit@gmail.com>
2018-09-19 22:47:17 +1000

    Models: User, Blog CSS: main, blog

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField(max_length=256, help_text="Имя пользователя")
+    surname = models.CharField(max_length=256, help_text="Фамилия пользователя")
+
+    def __str__(self):
+        return "%s %s"%(self.name, self.surname)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=256)
+    author = models.OneToOneField(User, models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
+    blog = models.TextField()
+
+    def __str__(self):
+        return self.title
+
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import *
 
 
 def index(request):
-    return render(request, "index.html")
+    forms = Article.objects.order_by('-date')
+    return render(request, "index.html", {'forms':forms})
